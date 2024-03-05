88c693c8b73edc8782d86263e67b60dd222740bd
xull <xull@servyou.com.cn>
2024-03-05 10:51:36 +0800

    add source

---
+++ src/main/java/com/alec/InnovateX/InnovateXApplication.java
@@ -0,0 +1,13 @@
+package com.alec.InnovateX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovateXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovateXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alec/InnovateX/InnovateXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alec.InnovateX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovateXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
