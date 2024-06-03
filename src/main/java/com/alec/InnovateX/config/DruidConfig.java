b2b04b0bf59b71de168d2df4368bedb14bf39518
xull <xull@servyou.com.cn>
2024-06-03 19:34:24 +0800

    新增druid数据源配置

---
+++ src/main/java/com/alec/InnovateX/config/DruidConfig.java
@@ -0,0 +1,18 @@
+package com.alec.InnovateX.config;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DataSource druidDataSource() {
+        return DruidDataSourceBuilder.create().build();
+    }
+}
