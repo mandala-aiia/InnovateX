9b70181b43d037ce4c222237e05034e8438f22e4
xull <xull@servyou.com.cn>
2024-12-04 15:53:22 +0800

    add springCode test

---
+++ src/main/java/com/alec/InnovateX/AppAbsServiceSecImpl.java
@@ -0,0 +1,9 @@
+package com.alec.InnovateX;
+
+public class AppAbsServiceSecImpl extends AppAbsService{
+
+    @Override
+    public void display() {
+        System.out.println("AppAbsServiceSecImpl.................display");
+    }
+}
