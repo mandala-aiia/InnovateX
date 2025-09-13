38fcf624aed5a2ab854a17e9758f18b2ede1d67e
Alec <mandala_aiia@163.com>
2025-09-13 17:58:59 +0800

    优化代码

--- src/main/java/com/alec/InnovateX/InnovateXApplication.java
+++ src/main/java/com/alec/InnovateX/InnovateXApplication.java
@@ -1,21 +1,13 @@
 package com.alec.InnovateX;
 
-import com.alibaba.druid.spring.boot3.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class,
-		DruidDataSourceAutoConfigure.class
-})
 
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class InnovateXApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InnovateXApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InnovateXApplication.class, args);
+    }
 }
